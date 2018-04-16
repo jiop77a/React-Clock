85ce2f87510af660962d9184d0bfac1a201c7cde
Alex E Scott <aescott@outlook.com>
2018-04-16 14:21:32 -0700

    format hours and meridian

--- frontend/root.jsx
+++ frontend/root.jsx
@@ -16,6 +16,7 @@ class Root extends React.Component {
     };
     this.increment = this.increment.bind(this);
     this.startClock = this.startClock.bind(this);
+    this.formatHours = this.formatHours.bind(this);
   }
 
   componentDidMount() {
@@ -27,11 +28,40 @@ class Root extends React.Component {
   }
 
   increment() {
-    let newTime = this.state.secs + 1;
-    this.setState({secs: newTime});
+    let newSecs = this.state.secs + 1;
+    let newMins = this.state.mins;
+    let newHours = this.state.hours;
+    if (newSecs >= 60) {
+      newSecs = 0;
+      newMins++;
+    }
+    if (newMins >= 60) {
+      newMins = 0;
+      newHours++;
+    }
+    if (newHours >= 24) {
+      newHours = 0;
+    }
+    this.setState({
+      secs: newSecs,
+      mins: newMins,
+      hours: newHours
+    });
+  }
+
+  formatHours(hours) {
+    if (hours === 0) {
+      return 12;
+    } else if (hours > 12) {
+      return hours - 12;
+    } else {
+      return hours;
+    }
   }
 
   render() {
+    let meridian = this.state.hours > 12 ? "PM" : "AM";
+    let bareHours = this.formatHours(this.state.hours);
 
 
     return(
@@ -40,7 +70,7 @@ class Root extends React.Component {
           React is up and running!
         </div>
         <div>
-          {this.state.hours}:{this.state.mins}:{this.state.secs}
+          {bareHours}:{this.state.mins}:{this.state.secs}{meridian}
         </div>
       </div>
     );
