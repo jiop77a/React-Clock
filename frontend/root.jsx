5c62706021be9e037097e78b605b4a690084b713
Alex E Scott <aescott@outlook.com>
2018-04-16 14:06:44 -0700

    get clock ticking

--- frontend/root.jsx
+++ frontend/root.jsx
@@ -5,10 +5,43 @@ import ReactDOM from 'react-dom';
 
 
 class Root extends React.Component {
+
+  constructor(props) {
+    super(props);
+    let date = new Date();
+    this.state = {
+      hours: date.getHours(),
+      mins: date.getMinutes(),
+      secs: date.getSeconds()
+    };
+    this.increment = this.increment.bind(this);
+    this.startClock = this.startClock.bind(this);
+  }
+
+  componentDidMount() {
+    this.startClock();
+  }
+
+  startClock() {
+    setInterval(this.increment, 1000);
+  }
+
+  increment() {
+    let newTime = this.state.secs + 1;
+    this.setState({secs: newTime});
+  }
+
   render() {
+
+
     return(
       <div>
-        React is up and running!
+        <div>
+          React is up and running!
+        </div>
+        <div>
+          {this.state.hours}:{this.state.mins}:{this.state.secs}
+        </div>
       </div>
     );
   }
